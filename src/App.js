58a2f190b329eceb0061eb8b2739b59eec243857
Taylor Bruening <taylorbruening@gmail.com>
2024-03-27 09:22:47 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import MainContent from './MainContent.js'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg" style={{height: '6vh'}}>
+        <Container height='5vh'>
+          <Navbar.Brand href="#home">6ix Trading</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div style={{ display: 'flex' }}>
+        <div style={{
+          width: '10vw',
+          height: '94vh',
+          backgroundColor: '#9c9c9c',
+          padding: '1rem'
+        }}>
+          Dashboard
+        </div>
+        <main style={{ flexGrow: '1', padding: '1rem' }}>
+          <MainContent />
+        </main>
+      </div>
+    </>
   );
 }
 
---
+++ src/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainContent() {
+
+  return (
+    <div>
+        Main content goes here
+    </div>
+  );
+}
+
+export default MainContent;
